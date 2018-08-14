420beeae9c518027b941d239bbd8952186a6bd6d
Meguru Yasuda <yasudamel@gmail.com>
2018-08-14 09:06:20 -0700

    add logit to js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,17 @@ $(document).ready(function(){
     event.preventDefault();
     var countTo = parseInt($("#countto").val());
     var countBy = parseInt($("#countby").val());
-    console.log(countTo);
-    console.log(countBy);
+
+    if (isNaN(countTo) || isNaN(countBy)) {
+      alert("put numbers");
+    }  else if (countTo < 0 || countBy < 0){
+        alert("put positive numbers");
+    } else if (countTo < countBy){
+        alert("Count by number needs to be smaller than the count to number");
+    } else {
+      for (var index = countBy; index <= countTo; index += countBy) {
+        $("#result").append(" " + index + " ");
+      }
+    }
   });
 });
